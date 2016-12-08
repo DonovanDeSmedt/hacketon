123a7dc61aba1dada73255c7f01e9ec0eda7f946
Arno Lambert <Arno@Arnos-MacBook-Pro-2.local>
2016-12-08 15:39:29 +0100

    map + detail view

--- app/scripts/rover/detailController.js
+++ app/scripts/rover/detailController.js
@@ -17,27 +17,47 @@
         self.listenToRoute = listenToRoute;
         self.rover;
         self.initMap = initMap;
+        self.getDirectionArrow = getDirectionArrow;
+
 
         function listenToRoute(){
             let id = $stateParams.id;
-            // $interval(function(){
+            $interval(function(){
                 channelFactory.getRoverById(id).then(function(data) {
                     self.rover = data;
+                    self.initMap();
                 });
-            // },1000);
+            },1000);
         }
         self.listenToRoute();
 
         function initMap(){
+
           var mapWidth = $('.map').width();
 					$('.map').css('height', mapWidth);
-          console.log(rover.position.x);
-          var roverX = rover.position.x;
-          var roverY = rover.position.y;
+          var roverX = self.rover.position.x;
+          var roverY = self.rover.position.y;
           roverX = Math.floor(roverX / 4);
           roverY = Math.floor(roverY / 4);
-					$('.map-marker').css('top', 'calc(' + roverY + ' - 13px)').css('left', 'calc(' + roverX + '- 7px)');
+					$('.map-marker').css('bottom', 'calc( 50% + ' + roverY + 'px - 13px)').css('left', 'calc( 50% + ' + roverX + 'px - 13px)');
         }
-        self.initMap();
+
+        function getDirectionArrow(direction) {
+          switch (direction) {
+            case 0:
+              return 'fa fa-arrow-left';
+              break;
+            case 1:
+              return 'fa fa-arrow-up';
+              break;
+            case 2:
+              return 'fa fa-arrow-right';
+              break;
+            case 3:
+              return 'fa fa-arrow-down';
+              break;
+          }
+        };
+
     };
 })();
